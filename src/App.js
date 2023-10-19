59f45c12d971adfcdc2512070c2ec4ed7bcfe40e
Hpatter9 <hpatter9@gmail.com>
2023-10-19 18:54:54 +0300

    highlights setion finished

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ import React from'react';
 import Footer from './Components/Footer';
 import Header from './Components/Header';
 import Hero from './Components/Hero';
+import Highlights from './Components/Highlights';
 import './Components/header.css'; // Import the CSS file for the Header component
 import './Components/footer.css';
 import './Components/hero.css';
+import './Components/highlights.css'
 import './App.css'
 
 const App = ()=> {
@@ -12,6 +14,7 @@ const App = ()=> {
     <body classname = 'app-body'>
       <Header className='header' />
       <Hero className='hero'/>
+      <Highlights className='highlights' />
       <Footer className='footer'/>
     </body>
   );
---
+++ src/Components/Highlights.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './highlights.css';
+
+const Hightlights = ()=> {
+    return (
+        <specials className ='specialsblock'>
+            <div className ="specials">
+                <h3>Specials</h3>
+                <button id="button">Online Menu</button>
+            </div>
+        </specials>
+    )
+}
+
+export default Hightlights;
